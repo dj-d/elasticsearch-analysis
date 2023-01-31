12eefa8ccb15c12281bfa8d4d2d58a4d82225d5e
Daniele Albanese <daniele.albanese98@gmail.com>
2023-02-01 00:56:35 +0100

    Added constants file

---
+++ constants.py
@@ -0,0 +1,8 @@
+PROJECT_PATH='/home/dj-d/Repositories/GitHub/elasticsearch/'
+TOOL_PATH='/home/dj-d/University/Automated_Software_Delivery/Exam/readability'
+
+START_COMMIT = 101
+END_COMMIT = 300
+
+FILES_LOCATION = '/home/dj-d/Repositories/GitHub/asd_exam'
+OUTPUT_JSON_NAME = 'output.json'
