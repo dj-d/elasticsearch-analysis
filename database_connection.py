681e4e5e644b1aaa0d14213d40dd15e07b62789f
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-02-02 18:19:39 +0100

    Inserimeto dei file JSON

--- database_connection.py
+++ database_connection.py
@@ -2,6 +2,7 @@
 Connessione al database MongoDB
 '''
 import json
+import os
 
 import pymongo
 from pymongo import MongoClient
@@ -18,10 +19,34 @@ except ConnectionFailure as e:
     print("Could not connect to MongoDB")
     print(e)
 
-with open("fileJSONProva.json") as file:
-    revision_history = json.load(file)
+#Path alla directory dei file
+results_directory = "Results"
+
 
 db = connect["Revision_History_DB"]
 commit_collection = db["Revision_Collection"]
-commit_collection.insert_many(revision_history)
+
+for results_commits_json in os.listdir(results_directory):
+    path_file_in_dir = os.path.join(results_directory, results_commits_json)
+    print("File_Name: " + results_commits_json + "  ---  File_Path: " + path_file_in_dir)
+
+    with open(path_file_in_dir) as json_file:
+        iters_json = json.load(json_file)
+        commit_collection.insert_many(iters_json)
+
+
+    #TODO: Vedi come si fanno le query
+
+
+
+#Trovo i file da inserire
+# with open("fileJSONProva.json") as file:
+#    revision_history = json.load(file)
+
+#Creo il database
+#db = connect["Revision_History_DB"]
+#commit_collection = db["Revision_Collection"]
+
+
+#commit_collection.insert_many(revision_history)
 
