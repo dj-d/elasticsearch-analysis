63ac841264ba61cfe76dafd57de62b3954daa9bf
Andrea D'Aguanno <daguannoandrea@outlook.com>
2023-02-12 16:09:42 +0100

    Refactoring: Log AND FUNCTIONS

--- database_connection.py
+++ database_connection.py
@@ -31,7 +31,7 @@ except ConnectionFailure as e:
 
 # Path alla directory dei file
 results_directory = "Results"
-#results_directory = "Results_Test"
+# results_directory = "Results_Test"
 
 db = client["Revision_History_DB"]
 # reference al database
@@ -49,44 +49,6 @@ if (Populate_DB):
             collection.insert_many(iters_json)
 
 
-def find_only_unreadable_file_byCommitID(commit_id):
-    """
-    :param  l'id di una commit
-    :return tutti i file unredable di un determianto commit, la query ritorna solamente il nome del file
-    """
-
-    cursor = collection.find_one({"revision_history":
-                                      {"$elemMatch": {"isReadable": False}},
-                                  "commit_id": commit_id},
-                                 {"revision_history.file_name": 1})
-
-    for i in cursor['revision_history']:
-        print(i)
-
-
-def find_file_NotReadable_occurrence(file_name):
-    """
-    :return Id delle commit ed il timestamp di tutte le occorrenze del file in cui risulta unredable
-    """
-    cursor = collection.find({"revision_history":
-                                  {"$elemMatch": {"$and": [{"file_name": file_name}, {"isReadable": False}]}},
-                              "revision_history.file_name": file_name},
-                             {"commit_id": 1, "timestamp": 1})
-
-    i = 1
-    for file_occurence in cursor:
-        print(str(i) + " ->" + "File not Readable in Commit: " + file_occurence['commit_id'] + "  ||  Timestamp: " +
-              file_occurence['timestamp'])
-        i = i + 1
-
-    # if((len(list(cursor))) == 0):
-    #    print("The file [" + file_name + "] is always readable")
-    # else:
-    #    for file_occurence in cursor:
-    #        print(file_occurence)
-    # print("Commit: " + file_occurence['commit_id'] + "  ||  Timestamp: " + file_occurence['timestamp'] + "  ||  isReadable: " + file_occurence['isReadable'])
-
-
 def get_all_commit_in_DB():
     """
     :return: la lista degli id dei commit presenti nel Database
@@ -97,18 +59,6 @@ def get_all_commit_in_DB():
         print(i["commit_id"])
 
     return list(cursor)
-
-
-def get_all_timestamps_in_DB():
-    cursor = collection.find({},
-                             {"timestamp": 1, "_id": 0}
-                             )
-
-    # for index, item in enumerate(cursor):
-    #   print(str(index) +" " + item['timestamp'])
-
-    for item in cursor:
-        print(item['timestamp'])
 
 
 def get_all_file():
@@ -164,21 +114,6 @@ def get_all_unreadable_file():
     return unique_unreadable_file_names
 
 
-def get_file_timestamps(file_name):
-    """
-    :param file_name il nome del file del quale vogliamo ottenre i timestemps
-    :return la lista dei timestamp dato un file in input
-    """
-    cursor = collection.find({"revision_history.file_name": file_name}, {"timestamp": 1, "_id": 0})
-    i = 1
-
-    for file_occurrence in cursor:
-        print(str(i) + " -> " + str(file_occurrence))
-        i = i + 1
-
-    return list(cursor)
-
-
 def get_file_timestamps_and_readable(file_name):
     """
     .$ elemento di proiezione
@@ -186,28 +121,6 @@ def get_file_timestamps_and_readable(file_name):
     :param file_name il nome del file del quale vogliamo ottenre i timestemps e la leggibilità relativa
     :return dizionario key:value  commit_timestamp : isReadable_value
     """
-    # cursor = collection.find({"revision_history.file_name": file_name},
-    #                          {"timestamp": 1, "commit_id": 1, "_id": 0, "revision_history.isReadable.$": 1}
-    #                          )
-    """
-    Nella query ritoriniamo il valore isUnure perché dato che in input diamoil nome del file, questo è già un file 
-    trovato con score minore di 0.4 quindi adesso portandoci questo booleano abbiamo che se è True allora il file è leggibile
-    oppure siamo insicuri che lo sia. Mentre è false solo nel caso in cui è minore di 0.4. 
-    Abbiamo tre casi: 
-    isReadable  :   True    False   False
-    isUnsure    :   False   False   True
-    Il ".$" in MongoDB è un operatore proiezione che viene utilizzato per identificare un singolo elemento dell'array corrispondente alla condizione specificata nel documento di query. In questo caso, la query restituirà il documento che ha un elemento "revision_history" con un "file_name" specificato che corrisponde alla variabile "file_name". Il documento di output conterrà solo le proprietà "timestamp", "commit_id" e "revision_history.isUnsure.$".
-    
-    
-    cursor = collection.find({"revision_history.file_name": file_name},
-                             {"timestamp": 1,
-                              "commit_id": 1,
-                              "_id": 0,
-                              "revision_history.$.isUnsure": 1,
-                              "revision_history.$.isReadable": 1}
-                             )
-    """
-
     pipeline = [
         {"$match": {"revision_history.file_name": file_name}},
         {"$unwind": "$revision_history"},
@@ -217,7 +130,7 @@ def get_file_timestamps_and_readable(file_name):
             "commit_id": 1,
             "isUnsure": "$revision_history.isUnsure",
             "isReadable": "$revision_history.isReadable"
-        }},{"$sort": {"timestamp": 1}}
+        }}, {"$sort": {"timestamp": 1}}
     ]
     cursor = collection.aggregate(pipeline)
 
@@ -242,21 +155,17 @@ def get_minutes_delta(first_timestamp, second_timestamp):
     '''
     delta_seconds = int(second_timestamp) - int(first_timestamp)
     delta_minutes = delta_seconds / 60
-    print("Delta: " + str(delta_minutes))
+    # print("Delta: " + str(delta_minutes))
     return delta_minutes
 
 
 def get_most_unreadable():
     # prende solo i file illegibili nel DB
+    print("Find all commits files...")
     all_unreadable_file_set = get_all_unreadable_file()
 
-    for file in all_unreadable_file_set:
-        #file = "modules/elasticsearch/src/main/java/org/elasticsearch/rest/action/search/RestSearchAction.java"
-        #file = "modules/elasticsearch/src/test/java/org/elasticsearch/transport/netty/SimpleNettyTransportTests.java"
-        #file ="src/main/java/org/elasticsearch/common/logging/log4j/LogConfigurator.java"
-        #file = "src/test/java/org/elasticsearch/index/mapper/binary/BinaryMappingTests.java"
-        #file = "src/test/java/org/elasticsearch/script/javascript/JavaScriptScriptSearchTests.java"
-
+    for analyzed_file_index, file in enumerate(all_unreadable_file_set):
+        print("Analyzing: (" + str(analyzed_file_index + 1) + "/" + str(len(all_unreadable_file_set)) + ") -> " + str(file))
         # otteniamo i valori del file analizzato
         file_details = get_file_timestamps_and_readable(str(file))
 
@@ -279,17 +188,11 @@ def get_most_unreadable():
         first_timestamp = 0
         second_timestamp = 0
         only_one = False
-        print(len(file_details['readability']))
         interval_files_dict[str(file)] = 0
         for index in range(len(file_details['readability'])):
-            print("- - - - index: " + str(index))
             if (file_details['readability'][index] == False) and (file_details['unsure'][index] == False):
-                print("-> False / False")
-                print(index)
-
                 if index == (len(file_details['readability']) - 1):
                     if len(file_details['readability']) == 1:
-                        print("Solo elemento")
                         # caso di un solo elemento nell'array
                         second_timestamp = 0
                     else:
@@ -300,32 +203,28 @@ def get_most_unreadable():
                             second_timestamp = file_details['timestamp'][index]
                             interval_files_dict[str(file)] += get_minutes_delta(first_timestamp=first_timestamp,
                                                                                 second_timestamp=second_timestamp)
-                    print(interval_files_dict)
+
 
                 elif first_timestamp == 0:
                     # primo caso
                     first_timestamp = file_details['timestamp'][index]
                     interval_files_dict[str(file)] += 0
                     only_one = True
-                    # TODO: CAso da valutare un solo Flse
 
                 else:
                     # false intermedio non si aggiunge
                     second_timestamp = file_details['timestamp'][index]
                     only_one = False
             elif (file_details['readability'][index] == False) and (file_details['unsure'][index] == True):
-                print("-> False / True")
-                if file_details['unsure'][index - 1] == False and only_one == False and index != 0 and first_timestamp != 0:
-                    print("Meno -1 " + str(file_details['unsure'][index - 1]) )
+                if file_details['unsure'][
+                    index - 1] == False and only_one == False and index != 0 and first_timestamp != 0:
                     # caso in cui siamo incerti e quindi bisogna prendere il precendente e calcoliamo
                     second_timestamp = file_details['timestamp'][index - 1]
                     interval_files_dict[str(file)] += get_minutes_delta(first_timestamp=first_timestamp,
                                                                         second_timestamp=second_timestamp)
                     first_timestamp = 0
                     second_timestamp = 0
-                    print(interval_files_dict)
                 elif only_one == True:
-                    print("only one")
                     # caso solo un false ed unsure True
                     second_timestamp = file_details['timestamp'][index]
                     interval_files_dict[str(file)] += get_minutes_delta(first_timestamp=first_timestamp,
@@ -334,7 +233,6 @@ def get_most_unreadable():
                     first_timestamp = 0
                     second_timestamp = 0
             elif (file_details['readability'][index] == True) and (file_details['unsure'][index] == False):
-                print("-> True / False")
                 if only_one == True:
                     # Solo un false ed unreadable True
                     second_timestamp = file_details['timestamp'][index]
@@ -343,20 +241,12 @@ def get_most_unreadable():
                     only_one = False
                     first_timestamp = 0
                     second_timestamp = 0
-            print("T1: " + str(first_timestamp) + "\n" + "T2: " + str(second_timestamp))
-       # break
 
-    for index in data_file_list:
-        print(index)
-    # for file_delta in interval_files_dict:
-    # print(interval_files_dict)
     sorted_files_deltaTime = sorted(interval_files_dict.items(), key=itemgetter(1), reverse=True)
 
     with open("output_files.csv", "w", newline="") as file:
         writer = csv.writer(file)
         for time in sorted_files_deltaTime:
-            print(time[1])
-            #file.write(str(time) + "\n")
             writer.writerow([time[0], time[1]])
 
 
@@ -367,7 +257,6 @@ def get_Commit_Author(commit_id):
     """
     cursor = collection.find_one({"commit_id": str(commit_id)}, {"_id": 0, "author_name": 1})
 
-    # print(cursor['author_name'])
     return cursor['author_name']
 
 
@@ -393,30 +282,20 @@ def get_most_unreadable_author():
     :return la funzione assegna agli autori che introducono file illegibili un intero che rappresenta il numero di file non leggibili introdotti in tutto il progetto
     """
 
-    authors_set = set()
     authors_dict = {}
-
+    print("\n" + "Get Most unreadable Authors...")
     for dict_file in data_file_list:
         for index in range(len(dict_file['readabilities_file'])):
-            print("=> " + str(index))
             '''
             Questo controllo è necessario perché ci servono solamente gli autori dei file che sono illegibli 
             sicuramente. Quindi sia isUnsure che isREadable sono false. 
             '''
             if ((dict_file['readabilities_file'][index] == False) and (dict_file['unsure'][index]) == False):
-                print("False    -> " + str(index + 1))
-                print("Commit_id->" + dict_file['commit_id_file'][index])
                 authors = get_Commit_Author(commit_id=dict_file['commit_id_file'][index])
-                print("Author   ->" + authors)
-                authors_set.add(authors)
-
                 if authors in authors_dict:
-                    print("ESISTE")
                     authors_dict[authors] = authors_dict[authors] + 1
                 else:
-                    print("NON ESISTE")
                     authors_dict[authors] = 1
-                print("- - - - - -")
                 # Break the iterating on the first appearance of the False value in a file's readability
                 break
 
@@ -424,11 +303,8 @@ def get_most_unreadable_author():
     authors_dict = sorted(authors_dict.items(), key=lambda x: x[1], reverse=True)
 
     # Stampa il dizionario degli autori con le occorrenze
-    print("Author_SET_Length: " + str(len(authors_set)))
     print("Author_DIC_Length: " + str(len(authors_dict)))
     print("\n")
-    # for key, value in authors_dict.items():
-    #    print(str(key) + " : " + str(value))
 
     with open("output_authors.csv", "w", newline="", encoding="utf-8") as file_auth:
         writer_auth = csv.writer(file_auth)
@@ -437,36 +313,5 @@ def get_most_unreadable_author():
             writer_auth.writerow([author[0], author[1]])
 
 
-
-# TODO: La logica dei false è sbagliata
-
-# find_only_unreadable_file_byCommitID("bd2b0a632bfc5aabb408e7f47cfaa52a7d1b2b50")
-# find_file_NotReadable_occurence("modules/elasticsearch/src/main/java/org/elasticsearch/action/bulk/TransportShardBulkAction.java")
-# get_all_commit_in_DB()
-# get_all_file()
-# get_all_file_timestamps("modules/benchmark/micro/src/main/java/org/elasticsearch/benchmark/index/engine/SimpleEngineBenchmark.java")
-# A = "modules/elasticsearch/src/main/java/org/elasticsearch/index/merge/policy/LogByteSizeMergePolicyProvider.java"
-# get_all_file_timestamps_and_readable(A)
-# find_file_NotReadable_occurrence(A)
-# get_all_unreadable_file()
-
-# get_all_file()
-# get_all_timestamps_in_DB()
-# get_all_commit_in_DB()
-# print all
-# get_Commit_Author()
-
-# get_all_authors()
-
-
 get_most_unreadable()
 get_most_unreadable_author()
-
-# CASO
-# src/main/java/org/elasticsearch/index/query/CustomFiltersScoreQueryParser.java
-# {author_name : "jayson.minard"}
-
-# plugins/cloud/aws/src/main/java/org/elasticsearch/discovery/ec2/Ec2Discovery.java
-# {author_name : "Paul_Loy"}
-
-# get_file_timestamps_and_readable("src/main/java/org/elasticsearch/index/query/FilterBuilders.java")
